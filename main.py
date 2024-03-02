a827f792ffc59243a4aa4964218ba6b1d0a916d5
Владислав <125855952+WladislawFilatew@users.noreply.github.com>
2024-03-02 13:43:13 +0300

    Update main.py
    problems

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ my_list2 = [1,2,3,4,5]
 my_list.sort() # ['cherry', 'fish', 'leaf']
 my_list1.sort() # ['A', 'B', 'C', 'D']
 print(sorted(my_list2, reverse=True)) # [5, 4, 3, 2, 1]
+a = 0
+b = 100 / a
